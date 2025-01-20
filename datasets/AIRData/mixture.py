26d956e32eeb7fb9e6efd7b126ca796acc6a8e52
zhengjinliang <zhengjl23@mails.tsinghua.edu.cn>
2025-01-20 17:35:21 +0800

    Init

---
+++ datasets/AIRData/mixture.py
@@ -0,0 +1,8 @@
+AIR_NAMED_MIXTURES = {
+    'uniact-1.0version':
+        [
+            ('libero', 2.0),
+            ('AirKitchen', 2.0)
+        ]
+    
+}
---
+++ models/model_config.py
@@ -0,0 +1,36 @@
+DATASETS_NAME_TO_INTERPRETER = {
+    "fractal20220817_data": "VinillaInterpreter",                # Google RT-1 Robot Data (Large-Scale)
+    "kuka": "VinillaInterpreter",
+    "bridge_dataset": "VinillaInterpreter",                                   # Original Version of Bridge V2 from Project Website
+    "taco_play": "VinillaInterpreter",
+    "jaco_play": "VinillaInterpreter",
+    "berkeley_cable_routing": "VinillaInterpreter",
+    "roboturk": "VinillaInterpreter",
+    "viola": "VinillaInterpreter",
+    "berkeley_autolab_ur5": "VinillaInterpreter",
+    "toto": "VinillaInterpreter",
+    "language_table": "VinillaInterpreter",
+    "stanford_hydra_dataset_converted_externally_to_rlds": "VinillaInterpreter",
+    "austin_buds_dataset_converted_externally_to_rlds": "VinillaInterpreter",
+    "nyu_franka_play_dataset_converted_externally_to_rlds": "VinillaInterpreter",
+    "furniture_bench_dataset_converted_externally_to_rlds": "VinillaInterpreter",
+    "ucsd_kitchen_dataset_converted_externally_to_rlds": "VinillaInterpreter",
+    "austin_sailor_dataset_converted_externally_to_rlds": "VinillaInterpreter",
+    "austin_sirius_dataset_converted_externally_to_rlds": "VinillaInterpreter",
+    "dlr_edan_shared_control_converted_externally_to_rlds": "VinillaInterpreter",
+    "iamlab_cmu_pickup_insert_converted_externally_to_rlds": "VinillaInterpreter",
+    "utaustin_mutex": "VinillaInterpreter",
+    "berkeley_fanuc_manipulation": "VinillaInterpreter",
+    "cmu_stretch": "VinillaInterpreter",
+    "bc_z": "VinillaInterpreter",                                          # Note: use v0.1.0 --> later versions broken
+    "fmb_dataset": "VinillaInterpreter",
+    "dobbe": "VinillaInterpreter",
+    "droid": "VinillaInterpreter",
+    
+    # libero simulation
+    'libero': "VinillaInterpreter",
+    
+    # air robot data
+    'AirKitchen': "VinillaInterpreter",
+    'AIRBOT_single': "VinillaInterpreter"
+}
